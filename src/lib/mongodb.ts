19815bb0cb9596b41f14ce193336d8f3e7258ebf
henry22 <henry811010@gmail.com>
2025-07-31 11:54:35 +1000

    fix typo

--- src/lib/mongodb.ts
+++ src/lib/mongodb.ts
@@ -1,6 +1,6 @@
 import { MongoClient } from "mongodb";
 
-const uri = process.env.MONGEDB_URI!;
+const uri = process.env.MONGODB_URI!;
 const options = { maxPoolSize: 10 };
 
 let client: MongoClient;
