55fa8e8abd8a388661174bb417b6bf5696838af1
henry22 <henry811010@gmail.com>
2025-07-31 12:06:48 +1000

    complete get post detail api

--- src/app/api/post/detail/route.ts
+++ src/app/api/post/detail/route.ts
@@ -1,6 +1,8 @@
 import { NextRequest } from "next/server";
 import { withApiHandler } from "@/utils/withApiHandler";
 import { error, success } from "@/utils/apiResponse";
+import clientPromise from "@/lib/mongodb";
+import { DB_NAME } from "@/config/constants";
 
 export const GET = withApiHandler(async (request: NextRequest) => {
   const { searchParams } = new URL(request.url);
@@ -12,5 +14,10 @@ export const GET = withApiHandler(async (request: NextRequest) => {
     });
   }
 
-  return Response.json(success({}));
+  const client = await clientPromise;
+  const db = client.db(DB_NAME);
+  const collection = db.collection("posts");
+  const post = await collection.findOne({ id });
+
+  return Response.json(success({ post }));
 });
