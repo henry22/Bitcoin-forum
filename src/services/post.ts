cb3f1767ee8fd7132383e51e9c6d6f82f38b2000
henry22 <henry811010@gmail.com>
2025-07-31 17:10:18 +1000

    fetch useQueryPostList api call in post-list

--- src/services/post.ts
+++ src/services/post.ts
@@ -1,5 +1,5 @@
 export const getPostList = async (page: number) => {
-  const response = await fetch(`/api/post/list?page=${page}&limit=10`);
+  const response = await fetch(`/api/post/list?page=${page}&limit=5`);
   const resData = await response.json();
 
   return resData.data;
